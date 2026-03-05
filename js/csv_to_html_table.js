89f859c4082b5d1c409f5b8bb095f0243c6916a3
Roshni Merugu <merugu.rosh@gmail.com>
2026-03-05 01:20:22 -0500

    pingpong + ty derek eder for pretty table

---
+++ js/csv_to_html_table.js
@@ -0,0 +1,84 @@
+var CsvToHtmlTable = CsvToHtmlTable || {};
+
+CsvToHtmlTable = {
+    init: function (options) {
+        options = options || {};
+        var csv_path = options.csv_path || "";
+        var el = options.element || "table-container";
+        var allow_download = options.allow_download || false;
+        var csv_options = options.csv_options || {};
+        var datatables_options = options.datatables_options || {};
+        var custom_formatting = options.custom_formatting || [];
+        var customTemplates = {};
+        $.each(custom_formatting, function (i, v) {
+            var colIdx = v[0];
+            var func = v[1];
+            customTemplates[colIdx] = func;
+        });
+
+        var $table = $("<table class='table table-striped table-condensed' id='" + el + "-table'></table>");
+        var $containerElement = $("#" + el);
+        $containerElement.empty().append($table);
+
+        $.when($.get(csv_path)).then(
+            function (data) {
+                var csvData = $.csv.toArrays(data, csv_options);
+
+                for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
+                    console.log(decryptAES(csvData[rowIdx][1]))
+                    csvData[rowIdx][1] = decryptAES(csvData[rowIdx][1])
+                    csvData[rowIdx][2] = decryptAES(csvData[rowIdx][2])
+                }
+                
+                var $tableHead = $("<thead></thead>");
+                var csvHeaderRow = csvData[0];
+                var $tableHeadRow = $("<tr></tr>");
+                for (var headerIdx = 0; headerIdx < csvHeaderRow.length; headerIdx++) {
+                    $tableHeadRow.append($("<th></th>").text(csvHeaderRow[headerIdx]));
+                }
+                $tableHead.append($tableHeadRow);
+
+                $table.append($tableHead);
+                var $tableBody = $("<tbody></tbody>");
+
+                for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
+                    var $tableBodyRow = $("<tr></tr>");
+                    for (var colIdx = 0; colIdx < csvData[rowIdx].length; colIdx++) {
+                        var $tableBodyRowTd = $("<td></td>");
+                        var cellTemplateFunc = customTemplates[colIdx];
+                        if (cellTemplateFunc) {
+                            $tableBodyRowTd.html(cellTemplateFunc(csvData[rowIdx][colIdx]));
+                        } else {
+                            $tableBodyRowTd.text(csvData[rowIdx][colIdx]);
+                        }
+                        $tableBodyRow.append($tableBodyRowTd);
+                        $tableBody.append($tableBodyRow);
+                    }
+                }
+                $table.append($tableBody);
+
+                $table.DataTable(datatables_options);
+
+                if (allow_download) {
+                    $containerElement.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
+                }
+            });
+        function decryptAES(encrypted_text) {
+            try {
+                var key = CryptoJS.enc.Utf8.parse("pingpong12345678");
+                
+                var decrypted = CryptoJS.AES.decrypt(encrypted_text, key, {
+                    mode: CryptoJS.mode.ECB,
+                    padding: CryptoJS.pad.NoPadding
+                });
+                
+                var plaintext = decrypted.toString(CryptoJS.enc.Utf8);
+                
+                return plaintext.trim();
+            } catch(e) {
+                console.error("Decryption failed:", e);
+                return encryptedText;
+            }
+        }
+    }
+};
